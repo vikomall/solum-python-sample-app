5a2d793b64f752b39d7667d01fb7cb7e67ee9ba5
Murali Allada <murali44@users.noreply.github.com>
2015-02-03 17:33:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class Root(object):
         return tmpl.render(params)
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                        'server.socket_port': int(os.environ.get('PORT', '5000'))})
+                        'server.socket_port': int(os.environ.get('PORT',
+                                                                 '5000'))})
 
 cherrypy.quickstart(Root())
